4d3f181a846bc84e2f92725ed1f6c7cec01107f4
AndrewTaker <n0thingg@yandex.ru>
2024-10-11 06:46:20 +0300

    working demo

--- flex-art.js
+++ flex-art.js
@@ -1,16 +1,4 @@
-document.title = "CHEBUREK";
-
 var meta = document.createElement('meta');
 meta.name = "viewport";
 meta.content = "width=device-width, initial-scale=1";
 document.head.appendChild(meta);
-
-
-const tables = document.querySelectorAll('*');
-tables.forEach(table => {
-    const attrs = Array.from(table.attributes)
-    attrs.forEach(attr => {
-        table.removeAttribute(attr.name);
-    })
-});
-
